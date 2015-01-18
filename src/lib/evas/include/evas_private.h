8bc3ef9877b5a22ac78abaea5df23b4a57e9b066
Cedric BAIL <cedric@osg.samsung.com>
2015-01-18 17:23:49 +0100

    evas: first implementation of an ector drawer inside Evas software engine.
    Note: this won't work with the OpenGL backend.

--- src/lib/evas/include/evas_private.h
+++ src/lib/evas/include/evas_private.h
@@ -6,7 +6,7 @@
 #endif
 
 #include <Eina.h>
-#include <eina_safety_checks.h>
+#include <Ector.h>
 
 #include "Evas.h"
 
@@ -717,6 +717,7 @@ struct _Evas_Public_Data
    struct {
       Evas_Module *module;
       Evas_Func *func;
+      Ector_Surface *surface;
       struct {
          void *output;
 
@@ -1332,6 +1333,9 @@ struct _Evas_Func
    void  (*texture_filter_set)           (void *data, void *texture, Evas_3D_Texture_Filter min, Evas_3D_Texture_Filter mag);
    void  (*texture_filter_get)           (void *data, void *texture, Evas_3D_Texture_Filter *min, Evas_3D_Texture_Filter *mag);
    void  (*texture_image_set)            (void *data, void *texture, void *image);
+
+   Ector_Surface *(*ector_get)           (void *data);
+   void  (*ector_draw)                   (void *data, void *context, void *surface, Ector_Renderer *r, Eina_Array *clips, int x, int y, Eina_Bool do_async);
 };
 
 struct _Evas_Image_Save_Func
