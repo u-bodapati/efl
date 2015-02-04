5eca3f33aa675742e47a85d1c659276c9484c5e8
Cedric BAIL <cedric@osg.samsung.com>
2015-02-04 16:29:39 +0100

    efl: add Efl.Gfx.Fill and migrate Evas.Image to it.
    Conflicts:
    
    	src/lib/efl/Efl.h

--- src/lib/efl/Efl.h
+++ src/lib/efl/Efl.h
@@ -34,16 +34,6 @@ extern "C"
 # endif
 #endif /* ! _WIN32 */
 
-#ifdef EFL_BETA_API_SUPPORT
-
-/* Interfaces */
-#include "interfaces/efl_control.eo.h"
-#include "interfaces/efl_file.eo.h"
-#include "interfaces/efl_image.eo.h"
-#include "interfaces/efl_player.eo.h"
-#include "interfaces/efl_text.eo.h"
-#include "interfaces/efl_text_properties.eo.h"
-
 /**
  * These values determine how the points are interpreted in a stream of points.
  *
@@ -146,10 +136,35 @@ typedef enum _Efl_Gfx_Gradient_Spread
   EFL_GFX_GRADIENT_SPREAD_LAST /**< End of enum value */
 } Efl_Gfx_Gradient_Spread;
 
+/**
+ * Type defining how an image content get filled.
+ * @since 1.13
+ */
+typedef enum _Efl_Gfx_Fill_Spread
+{
+  EFL_GFX_FILL_REFLECT = 0, /**< image fill tiling mode - tiling reflects */
+  EFL_GFX_FILL_REPEAT = 1,  /**< tiling repeats */
+  EFL_GFX_FILL_RESTRICT = 2, /**< tiling clamps - range offset ignored */
+  EFL_GFX_FILL_RESTRICT_REFLECT = 3, /**< tiling clamps and any range offset reflects */
+  EFL_GFX_FILL_RESTRICT_REPEAT = 4, /**< tiling clamps and any range offset repeats */
+  EFL_GFX_FILL_PAD = 5 /**< tiling extends with end values */
+} Efl_Gfx_Fill_Spread;
+
+#ifdef EFL_BETA_API_SUPPORT
+
+/* Interfaces */
+#include "interfaces/efl_control.eo.h"
+#include "interfaces/efl_file.eo.h"
+#include "interfaces/efl_image.eo.h"
+#include "interfaces/efl_player.eo.h"
+#include "interfaces/efl_text.eo.h"
+#include "interfaces/efl_text_properties.eo.h"
+
 #include "interfaces/efl_gfx_utils.h"
 
 #include "interfaces/efl_gfx_base.eo.h"
 #include "interfaces/efl_gfx_stack.eo.h"
+#include "interfaces/efl_gfx_fill.eo.h"
 #include "interfaces/efl_gfx_shape.eo.h"
 #include "interfaces/efl_gfx_gradient.eo.h"
 #include "interfaces/efl_gfx_gradient_linear.eo.h"
