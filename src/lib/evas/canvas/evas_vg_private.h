648275b1540fe301e3b7879894bde55dad5c9513
Cedric BAIL <cedric@osg.samsung.com>
2015-02-07 02:29:24 +0100

    evas: move Evas.VG_* to Efl.VG.*
    This also introduce legacy API for all of them.
    
    Conflicts:
    
    	src/lib/evas/Evas_Legacy.h
    	src/lib/evas/canvas/efl_vg_base.eo
    	src/lib/evas/canvas/efl_vg_shape.eo
    	src/lib/evas/canvas/evas_object_vg.c
    	src/lib/evas/canvas/evas_vg_container.c
    	src/lib/evas/canvas/evas_vg_gradient.c
    	src/lib/evas/canvas/evas_vg_gradient_linear.c
    	src/lib/evas/canvas/evas_vg_gradient_radial.c
    	src/lib/evas/canvas/evas_vg_image.c
    	src/lib/evas/canvas/evas_vg_node.c
    	src/lib/evas/canvas/evas_vg_private.h
    	src/lib/evas/canvas/evas_vg_shape.c

--- src/lib/evas/canvas/evas_vg_private.h
+++ src/lib/evas/canvas/evas_vg_private.h
@@ -3,18 +3,18 @@
 
 #include <Ector.h>
 
-typedef struct _Evas_VG_Node_Data Evas_VG_Node_Data;
-typedef struct _Evas_VG_Container_Data Evas_VG_Container_Data;
-typedef struct _Evas_VG_Gradient_Data Evas_VG_Gradient_Data;
+typedef struct _Efl_VG_Base_Data Efl_VG_Base_Data;
+typedef struct _Efl_VG_Container_Data Efl_VG_Container_Data;
+typedef struct _Efl_VG_Gradient_Data Efl_VG_Gradient_Data;
 
-struct _Evas_VG_Node_Data
+struct _Efl_VG_Base_Data
 {
    Eina_Matrix3 *m;
-   Evas_VG_Node *mask;
+   Efl_VG *mask;
    Ector_Renderer *renderer;
    Evas_VG *eo_vg;
 
-   void (*render_pre)(Eo *obj, Eina_Matrix3 *parent, Ector_Surface *s, void *data, Evas_VG_Node_Data *nd);
+   void (*render_pre)(Eo *obj, Eina_Matrix3 *parent, Ector_Surface *s, void *data, Efl_VG_Base_Data *nd);
    void *data;
 
    double x, y;
@@ -24,12 +24,12 @@ struct _Evas_VG_Node_Data
    Eina_Bool changed : 1;
 };
 
-struct _Evas_VG_Container_Data
+struct _Efl_VG_Container_Data
 {
    Eina_List *children;
 };
 
-struct _Evas_VG_Gradient_Data
+struct _Efl_VG_Gradient_Data
 {
    // FIXME: Later on we should deduplicate it somehow (Using Ector ?).
    Efl_Gfx_Gradient_Stop *colors;
@@ -38,27 +38,27 @@ struct _Evas_VG_Gradient_Data
    Efl_Gfx_Gradient_Spread s;
 };
 
-static inline Evas_VG_Node_Data *
-_evas_vg_render_pre(Evas_VG_Node *node, Ector_Surface *s, Eina_Matrix3 *m)
+static inline Efl_VG_Base_Data *
+_evas_vg_render_pre(Efl_VG *child, Ector_Surface *s, Eina_Matrix3 *m)
 {
-   Evas_VG_Node_Data *nd = NULL;
-
-   if (!node) return EINA_FALSE;
+   Efl_VG_Base_Data *child_nd = NULL;
 
    // FIXME: Prevent infinite loop
-   nd = eo_data_scope_get(node, EVAS_VG_NODE_CLASS);
-   if (nd->render_pre) nd->render_pre(node, m, s, nd->data, nd);
+   if (child)
+     child_nd = eo_data_scope_get(child, EFL_VG_BASE_CLASS);
+   if (child_nd)
+     child_nd->render_pre(child, m, s, child_nd->data, child_nd);
 
-   return nd;
+   return child_nd;
 }
 
 static inline void
-_evas_vg_node_changed(Eo *obj)
+_efl_vg_base_changed(Eo *obj)
 {
    eo_do(obj, eo_event_callback_call(EFL_GFX_CHANGED, NULL));
 }
 
-#define EVAS_VG_COMPUTE_MATRIX(Current, Parent, Nd)              \
+#define EFL_VG_COMPUTE_MATRIX(Current, Parent, Nd)               \
   Eina_Matrix3 *Current = Nd->m;                                 \
   Eina_Matrix3 _matrix_tmp;                                      \
                                                                  \
