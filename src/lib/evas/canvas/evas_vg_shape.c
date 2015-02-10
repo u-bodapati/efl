23f6e759b03fd48e9aed7c6d765b4c46ac00037f
Cedric BAIL <cedric@osg.samsung.com>
2015-02-10 14:36:26 +0100

    ector: implementation of some common code in Ector.Renderer.Cairo.Base.
    This require to make some class mixin instead of interface or abstract. So a lot
    of class renaming is also necessary here.

--- src/lib/evas/canvas/evas_vg_shape.c
+++ src/lib/evas/canvas/evas_vg_shape.c
@@ -288,7 +288,7 @@ _efl_vg_shape_render_pre(Eo *obj EINA_UNUSED,
 
    if (!nd->renderer)
      {
-        nd->renderer = eo_do(s, ector_surface_renderer_factory_new(ECTOR_RENDERER_GENERIC_SHAPE_CLASS));
+        nd->renderer = eo_do(s, ector_surface_renderer_factory_new(ECTOR_RENDERER_GENERIC_SHAPE_MIXIN));
      }
 
    eo_do(nd->renderer,
