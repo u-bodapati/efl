2cc4c0efa7f3e7e1fe325521dbe05c8014d2976e
Cedric BAIL <cedric@osg.samsung.com>
2015-06-05 22:24:31 +0200

    evg: start implementing Efl_VG_Loader.

--- src/lib/evg/evg_main.c
+++ src/lib/evg/evg_main.c
@@ -20,11 +20,20 @@
 #endif
 
 #include <Evg.h>
+#include <Evas.h>
+
 #include "evg_private.h"
 
 typedef struct _Evg Evg;
 struct _Evg
 {
+   Eina_File *f;
+
+   Efl_VG_Container *start, *end;
+
+   Efl_VG_Container *self;
+
+   double pos;
 };
 
 int _evg_log_dom_global = 0;
@@ -85,64 +94,132 @@ evg_shutdown(void)
    return _evg_main_count;
 }
 
-static void
-_efl_vg_loader_start_set(Eo *obj, Evg *pd, const Efl_VG_Container *tree)
+static Efl_VG_Container *
+_efl_vg_loader_self_build(Efl_VG_Container *parent,
+                          const Efl_VG_Container *start,
+                          const Efl_VG_Container *end)
 {
+   Eina_Iterator *it;
+   Eo *start_child, *r;
+
+   if (!start || !end) return NULL;
+
+   r = eo_add(EFL_VG_CONTAINER_CLASS, parent);
+
+   it = eo_do_ret(start, it, efl_vg_container_children_get());
+
+   EINA_ITERATOR_FOREACH(it, start_child)
+     {
+        const char *name;
+        Eo *end_child;
+
+        name = eo_do_ret(start_child, name, efl_vg_name_get());
+
+        end_child = eo_do_ret(end, end_child, efl_vg_container_child_get(name));
+
+        if (!end_child) continue ;
+
+        if (eo_class_get(start_child) != eo_class_get(end_child)) continue;
+
+        if (eo_isa(start_child, EFL_VG_CONTAINER_CLASS))
+          _efl_vg_loader_self_build(r, start_child, end_child);
+        else
+          eo_add(eo_class_get(start_child), r);
+     }
+
+   return r;
+}
+
+static void
+_efl_vg_loader_start_set(Eo *obj EINA_UNUSED, Evg *pd, const Efl_VG_Container *tree)
+{
+   _eo_ref_replace(&pd->start, tree);
 }
 
 static const Efl_VG_Container *
-_efl_vg_loader_start_get(Eo *obj, Evg *pd)
+_efl_vg_loader_start_get(Eo *obj EINA_UNUSED, Evg *pd)
 {
+   return pd->start;
 }
 
 static void
-_efl_vg_loader_end_set(Eo *obj, Evg *pd, const Efl_VG_Container *tree)
+_efl_vg_loader_end_set(Eo *obj EINA_UNUSED, Evg *pd, const Efl_VG_Container *tree)
 {
+   _eo_ref_replace(&pd->end, tree);
 }
 
 static const Efl_VG_Container *
-_efl_vg_loader_end_get(Eo *obj, Evg *pd)
+_efl_vg_loader_end_get(Eo *obj EINA_UNUSED, Evg *pd)
 {
+   return pd->end;
 }
 
 static void
-_efl_vg_loader_interpolation_set(Eo *obj, Evg *pd, double pos)
+_efl_vg_loader_interpolation_set(Eo *obj EINA_UNUSED, Evg *pd, double pos)
 {
+   pd->pos = pos;
+
+   if (!pd->self) pd->self = _efl_vg_loader_self_build(NULL, pd->start, pd->end);
+   // Walk self and access start and end to do the interpolation
 }
 
 static double
-_efl_vg_loader_interpolation_get(Eo *obj, Evg *pd)
+_efl_vg_loader_interpolation_get(Eo *obj EINA_UNUSED, Evg *pd)
 {
+   return pd->pos;
 }
 
 static Eo_Base *
 _efl_vg_loader_eo_base_constructor(Eo *obj, Evg *pd)
 {
+   return obj;
 }
 
 static void
 _efl_vg_loader_eo_base_destructor(Eo *obj, Evg *pd)
 {
+   eo_unref(pd->start);
+   eo_unref(pd->end);
+   eo_unref(pd->self);
+
+   eina_file_close(pd->f);
 }
 
 static Eina_Bool
 _efl_vg_loader_efl_file_file_set(Eo *obj, Evg *pd, const char *file, const char *key)
 {
+   Eina_File *tmp;
+   Eina_Bool r;
+
+   tmp = eina_file_open(file, EINA_FALSE);
+   if (!tmp) return EINA_FALSE;
+
+   r = eo_do_ret(obj, r, efl_file_mmap_set(tmp, key));
+
+   eina_file_close(tmp);
+   return r;
 }
 
 static void
 _efl_vg_loader_efl_file_file_get(Eo *obj, Evg *pd, const char **file, const char **key)
 {
+   if (!pd->f) return ;
+
+   if (file) *file = eina_file_filename_get(pd->f);
+   if (key) *key = NULL;
 }
 
 static Eina_Bool
 _efl_vg_loader_efl_file_mmap_set(Eo *obj, Evg *pd, const Eina_File *f, const char *key)
 {
+   // Only handling SVG for now
 }
 
 static void
 _efl_vg_loader_efl_file_mmap_get(Eo *obj, Evg *pd, const Eina_File **f, const char **key)
 {
+   if (f) *f = pd->f;
+   if (key) *key = NULL;
 }
 
 #include "efl_vg_loader.eo.c"
