aca5c5d8e578adf1d03918f1d5b3feb2db2e376f
Chris Michael <cpmichael@osg.samsung.com>
2016-02-05 13:21:40 -0500

    ecore-drm2: Support drmModeAddFB2 when creating framebuffer objects
    Signed-off-by: Chris Michael <cpmichael@osg.samsung.com>

--- src/lib/ecore_drm2/ecore_drm2_fb.c
+++ src/lib/ecore_drm2/ecore_drm2_fb.c
@@ -1,6 +1,34 @@
 #include "ecore_drm2_private.h"
 #include <sys/mman.h>
 
+static Eina_Bool
+_fb2_create(Ecore_Drm2_Fb *fb)
+{
+   struct drm_mode_fb_cmd2 cmd;
+   uint32_t hdls[4], pitches[4], offsets[4];
+
+   hdls[0] = fb->hdl;
+   pitches[0] = fb->stride;
+   offsets[0] = 0;
+
+   memset(&cmd, 0, sizeof(struct drm_mode_fb_cmd2));
+   cmd.fb_id = 0;
+   cmd.width = fb->w;
+   cmd.height = fb->h;
+   cmd.pixel_format = fb->format;
+   cmd.flags = 0;
+   memcpy(cmd.handles, hdls, 4 * sizeof(hdls[0]));
+   memcpy(cmd.pitches, pitches, 4 * sizeof(pitches[0]));
+   memcpy(cmd.offsets, offsets, 4 * sizeof(offsets[0]));
+
+   if (drmIoctl(fb->fd, DRM_IOCTL_MODE_ADDFB2, &cmd))
+     return EINA_FALSE;
+
+   fb->id = cmd.fb_id;
+
+   return EINA_TRUE;
+}
+
 EAPI Ecore_Drm2_Fb *
 ecore_drm2_fb_create(int fd, int width, int height, int depth, int bpp, unsigned int format)
 {
@@ -34,12 +62,15 @@ ecore_drm2_fb_create(int fd, int width, int height, int depth, int bpp, unsigned
    fb->size = carg.size;
    fb->stride = carg.pitch;
 
-   ret =
-     drmModeAddFB(fd, width, height, depth, bpp, fb->stride, fb->hdl, &fb->id);
-   if (ret)
+   if (!_fb2_create(fb))
      {
-        ERR("Could not add framebuffer: %m");
-        goto add_err;
+        ret =
+          drmModeAddFB(fd, width, height, depth, bpp, fb->stride, fb->hdl, &fb->id);
+        if (ret)
+          {
+             ERR("Could not add framebuffer: %m");
+             goto add_err;
+          }
      }
 
    memset(&marg, 0, sizeof(struct drm_mode_map_dumb));
