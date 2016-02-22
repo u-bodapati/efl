269e08cbc80b51ef94a6c5711d512b3dd31159eb
Chris Michael <cpmichael@osg.samsung.com>
2016-02-22 13:58:53 -0500

    ecore-drm2: Add API function to return the buffer id of an output's crtc
    Signed-off-by: Chris Michael <cpmichael@osg.samsung.com>

--- src/lib/ecore_drm2/ecore_drm2_outputs.c
+++ src/lib/ecore_drm2/ecore_drm2_outputs.c
@@ -517,3 +517,10 @@ ecore_drm2_output_vblank_get(Ecore_Drm2_Output *output)
 
    return 0;
 }
+
+EAPI int
+ecore_drm2_output_buffer_id_get(Ecore_Drm2_Output *output)
+{
+   EINA_SAFETY_ON_NULL_RETURN_VAL(output, 0);
+   return output->ocrtc->buffer_id;
+}
