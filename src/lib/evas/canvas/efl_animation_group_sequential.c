4b27138f5e8e95dd23a7a26fdbc5f346d6391196
Jaehyun Cho <jae_hyun.cho@samsung.com>
2017-06-01 22:03:25 +0900

    efl_animation: Remove instance methods from animation class

--- src/lib/evas/canvas/efl_animation_group_sequential.c
+++ src/lib/evas/canvas/efl_animation_group_sequential.c
@@ -23,66 +23,7 @@ do { \
 
 struct _Evas_Object_Animation_Group_Sequential_Data
 {
-   int current_index;
 };
-
-static Eina_Bool _index_animation_start(Eo *eo_obj, int index);
-
-static void
-_post_end_cb(void *data, const Efl_Event *event)
-{
-   Eo *eo_obj = data;
-
-   EFL_ANIMATION_GROUP_SEQUENTIAL_DATA_GET(eo_obj, pd);
-
-   efl_event_callback_del(event->object, EFL_ANIMATION_EVENT_POST_END,
-                          _post_end_cb, eo_obj);
-
-   pd->current_index++;
-
-   Eina_List *animations = efl_animation_group_animations_get(eo_obj);
-   if (eina_list_count(animations) == pd->current_index)
-     {
-        pd->current_index = 0;
-
-        //pre end event is supported within class only (protected event)
-        efl_event_callback_call(eo_obj, EFL_ANIMATION_EVENT_PRE_END, NULL);
-        efl_event_callback_call(eo_obj, EFL_ANIMATION_EVENT_END, NULL);
-        //post end event is supported within class only (protected event)
-        efl_event_callback_call(eo_obj, EFL_ANIMATION_EVENT_POST_END, NULL);
-
-        return;
-     }
-
-   _index_animation_start(eo_obj, pd->current_index);
-}
-
-static Eina_Bool
-_index_animation_start(Eo *eo_obj, int index)
-{
-   Efl_Animation *anim =
-      eina_list_nth(efl_animation_group_animations_get(eo_obj), index);
-   if (!anim || efl_animation_is_deleted(anim))
-     return EINA_FALSE;
-
-   //post end event is supported within class only (protected event)
-   efl_event_callback_add(anim, EFL_ANIMATION_EVENT_POST_END, _post_end_cb,
-                          eo_obj);
-
-   return efl_animation_start(anim);
-}
-
-EOLIAN static Eina_Bool
-_efl_animation_group_sequential_efl_animation_start(Eo *eo_obj, Evas_Object_Animation_Group_Sequential_Data *pd)
-{
-   EFL_ANIMATION_GROUP_SEQUENTIAL_CHECK_OR_RETURN(eo_obj, EINA_FALSE);
-
-   pd->current_index = 0;
-
-   efl_event_callback_call(eo_obj, EFL_ANIMATION_EVENT_START, NULL);
-
-   return _index_animation_start(eo_obj, pd->current_index);
-}
 
 EOLIAN static Efl_Animation *
 _efl_animation_group_sequential_efl_animation_dup(Eo *eo_obj, Evas_Object_Animation_Group_Sequential_Data *pd EINA_UNUSED)
--- src/lib/evas/canvas/efl_animation_scale.c
+++ src/lib/evas/canvas/efl_animation_scale.c
@@ -30,8 +30,6 @@ struct _Evas_Object_Animation_Scale_Data
 {
    Evas_Object_Animation_Scale_Property from;
    Evas_Object_Animation_Scale_Property to;
-
-   Evas_Map *map;
 };
 
 EOLIAN static void
@@ -155,97 +153,6 @@ _efl_animation_scale_efl_animation_dup(Eo *eo_obj, Evas_Object_Animation_Scale_D
    return animation;
 }
 
-static Evas_Map *
-_map_dup(Eo *target)
-{
-   if (!target) return NULL;
-
-   Evas_Map *map = NULL;
-
-   if (evas_object_map_enable_get(target) && evas_object_map_get(target))
-     {
-        map = evas_map_dup(evas_object_map_get(target));
-        if (!map) return NULL;
-     }
-   else
-     {
-        map = evas_map_new(4);
-        if (!map) return NULL;
-        evas_map_util_points_populate_from_object_full(map, target, 0);
-     }
-
-   return map;
-}
-
-static void
-_map_init(Evas_Map *map)
-{
-   evas_map_util_object_move_sync_set(map, EINA_TRUE);
-
-   //FIXME: Need to consider smooth of map
-}
-
-static void
-_pre_start_cb(void *data EINA_UNUSED, const Efl_Event *event)
-{
-   EFL_ANIMATION_SCALE_DATA_GET(event->object, pd);
-
-   Eo *target = efl_animation_target_get(event->object);
-   if (!target) return;
-
-   pd->map = _map_dup(target);
-   if (!pd->map) return;
-
-   _map_init(pd->map);
-}
-
-static void
-_pre_animate_cb(void *data EINA_UNUSED, const Efl_Event *event)
-{
-   EFL_ANIMATION_SCALE_DATA_GET(event->object, pd);
-   Efl_Animation_Animate_Event_Info *event_info = event->info;
-
-   double progress = event_info->progress;
-
-   Eo *target = efl_animation_target_get(event->object);
-   if (!target) return;
-
-   double scale_x =
-      (pd->from.scale_x * (1.0 - progress)) + (pd->to.scale_x * progress);
-
-   double scale_y =
-      (pd->from.scale_y * (1.0 - progress)) + (pd->to.scale_y * progress);
-
-   if (!pd->map)
-     {
-        pd->map = _map_dup(target);
-        if (!pd->map) return;
-
-        _map_init(pd->map);
-     }
-
-   Evas_Map *map = evas_map_dup(pd->map);
-
-   Evas_Coord x, y, w, h;
-   evas_object_geometry_get(target, &x, &y, &w, &h);
-
-   evas_map_util_zoom(map, scale_x, scale_y, x + (w / 2), y + (h / 2));
-
-   evas_object_map_set(target, map);
-   evas_object_map_enable_set(target, EINA_TRUE);
-
-   evas_map_free(map);
-}
-
-static void
-_pre_end_cb(void *data EINA_UNUSED, const Efl_Event *event)
-{
-   EFL_ANIMATION_SCALE_DATA_GET(event->object, pd);
-
-   evas_map_free(pd->map);
-   pd->map = NULL;
-}
-
 EOLIAN static Efl_Object *
 _efl_animation_scale_efl_object_constructor(Eo *eo_obj, Evas_Object_Animation_Scale_Data *pd)
 {
@@ -254,15 +161,6 @@ _efl_animation_scale_efl_object_constructor(Eo *eo_obj, Evas_Object_Animation_Sc
    pd->from.scale_x = 1.0;
    pd->from.scale_y = 1.0;
    pd->from.scale_z = 1.0;
-
-   //pre start event is supported within class only (protected event)
-   efl_event_callback_add(eo_obj, EFL_ANIMATION_EVENT_PRE_START, _pre_start_cb, NULL);
-
-   //pre animate event is supported within class only (protected event)
-   efl_event_callback_add(eo_obj, EFL_ANIMATION_EVENT_PRE_ANIMATE, _pre_animate_cb, NULL);
-
-   //pre end event is supported within class only (protected event)
-   efl_event_callback_add(eo_obj, EFL_ANIMATION_EVENT_PRE_END, _pre_end_cb, NULL);
 
    return eo_obj;
 }
